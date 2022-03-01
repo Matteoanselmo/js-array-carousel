3223e53ebf26e866b0d37758761f9de90f6bd448
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-01 16:45:27 +0100

    aggiunto filtro lista immagini

--- js/script.js
+++ js/script.js
@@ -6,9 +6,41 @@ for( let i = 0; i < images.length; i++){
     carouselContent += `
         <div class="col-10 carousel-element">
             <img src="img/${images[i]}" alt="random picture">
-        </div>`
+        </div>`;
 };
 
 const carouselWrapper = document.querySelector('div.my-first-row');
 carouselWrapper.innerHTML += carouselContent;
 
+const carouselElement = document.getElementsByClassName('carousel-element');
+carouselElement[0].classList.add('active');
+
+const imagesCarouselList = document.getElementsByClassName('my-img-list');
+imagesCarouselList[0].classList.add('my-padding-carousel');
+imagesCarouselList[0].classList.remove('my-brightness');
+
+
+const bottomButton = document.querySelector('button.my-bottom-btn');
+
+let elementActive = 0;
+
+bottomButton.addEventListener('click' , function() {
+    imagesCarouselList[elementActive].classList.add('my-brightness');
+    imagesCarouselList[elementActive].classList.remove('my-padding-carousel');
+    carouselElement[elementActive].classList.remove('active');
+    elementActive++;
+    imagesCarouselList[elementActive].classList.remove('my-brightness');
+    imagesCarouselList[elementActive].classList.add('my-padding-carousel');
+    carouselElement[elementActive].classList.add('active');
+});
+
+const topButton = document.querySelector('button.my-top-btn');
+topButton.addEventListener('click' , function() {
+    imagesCarouselList[elementActive].classList.add('my-brightness');
+    imagesCarouselList[elementActive].classList.remove('my-padding-carousel');
+    carouselElement[elementActive].classList.remove('active');
+    elementActive--;
+    imagesCarouselList[elementActive].classList.remove('my-brightness');
+    carouselElement[elementActive].classList.add('active');
+    imagesCarouselList[elementActive].classList.add('my-padding-carousel');
+});
